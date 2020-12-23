061d44fb7bdeaeac6086f479b1af0f53a12c1acc
plsmith <paul.smith1@mac.com>
2020-12-23 15:11:41 +0000

    pulling in the video stream

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import PlayerView from "react-native-aws-ivs-player-view";
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <script src="https://player.live-video.net/1.2.0/amazon-ivs-player.min.js" />
-          <video id="video-player" playsInline />
-          <script src="./script.js" />
-        </header>
+        <PlayerView
+            style={styles.player}
+            ref={(e: any) => {
+              this.player = e;
+            }}
+        />
+        <Button
+            onPress={() => {
+              this.player.pause();
+            }}
+            title="Pause"
+        />
+        <Button
+            onPress={() => {
+              this.player.load(
+                  'https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8'
+              );
+            }}
+            title="Play Me"
+        />
       </div>
     );
   }
