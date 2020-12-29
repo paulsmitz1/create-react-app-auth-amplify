c47d9768fb7ecd19d864f704c855e4fc7a722f36
plsmith <paul.smith1@mac.com>
2020-12-29 15:39:59 +0000

    wiping out amplify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify from "@aws-amplify/core";
-import aws_exports from './aws-exports';
-import $ from 'jquery';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import awsvideoconfig from "./aws-video-exports";
-import awsmobile from "./aws-exports";
+import Auth from "@aws-amplify/auth";
 
-Amplify.configure(aws_exports);
+Amplify.configure(awsconfig);
 
 let theStreams = [ "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8", awsvideoconfig.awsOutputLiveLL ];
 
@@ -43,10 +42,17 @@ class App extends Component {
       this.getStreams();
   }
 
-    getStreams(){
-      $.getJSON(awsmobile.aws_cloud_logic_custom.find( Name => "Streams").endpoint + "/streams")
-          .then(({ results }) => this.setState({ person: results }))
-  }
+    async getStreams() {
+        const apiName = 'Streams';
+        const path = '/Streams';
+        const myInit = {
+            headers: {
+                //Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
+            },
+        };
+
+        return await API.get(apiName, path, myInit);
+    }
 }
 
 export default withAuthenticator(App, true);
