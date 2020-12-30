9f677632b3bd64721732685ded3184d8fa00dffd
plsmith <paul.smith1@mac.com>
2020-12-30 11:03:51 +0000

    Fixing the video hopefully

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Amplify, { API } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import awsvideoconfig from "./aws-video-exports";
 import Auth from "@aws-amplify/auth";
-
 Amplify.configure(awsconfig);
 
 let theStreams = [ "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8", awsvideoconfig.awsOutputLiveLL ];
@@ -23,8 +22,8 @@ class App extends Component {
               data-setup='{}'
               autoPlay={true}
               controls>
-              <source src={ theStreams[0] }
-                  type="application/x-mpegURL" />
+              <source src='https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
+                  type='application/x-mpegURL' />
               <p className="vjs-no-js">
                   To view this video please enable JavaScript, and consider upgrading to a
                   web browser that
