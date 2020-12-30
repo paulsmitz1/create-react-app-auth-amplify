5111222ab9b81cae70f2c6aab754e6a9dd8c4a30
plsmith <paul.smith1@mac.com>
2020-12-30 15:40:27 +0000

    Allowing video switching!

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//Imports
 import React, { Component } from 'react';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
@@ -5,13 +6,21 @@ import Amplify, { API } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import awsvideoconfig from "./aws-video-exports";
 import Auth from "@aws-amplify/auth";
+import Select from 'react-select'
+
+let theStreams = [ "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8", "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8", "https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8" ];
+let options = [];
+let selectedStream = theStreams[0];
+
+//Configure
 Amplify.configure(awsconfig);
 
-let theStreams = [ "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8", awsvideoconfig.awsOutputLiveLL ];
+//Components
 
 class App extends Component {
 
     render() {
+        console.log("rendering, selectedStream: " + selectedStream);
 
       return (
       <div className="App">
@@ -19,10 +28,9 @@ class App extends Component {
               id="myplayer"
               className="video"
               preload="auto"
-              data-setup='{}'
-              autoPlay={true}
-              controls>
-              <source src='https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
+              autoPlay={"autoplay"}
+              controls={"controls"}>
+              <source id="source" src={ selectedStream }
                   type='application/x-mpegURL' />
               <p className="vjs-no-js">
                   To view this video please enable JavaScript, and consider upgrading to a
@@ -32,13 +40,40 @@ class App extends Component {
                   </a>
               </p>
           </video>
+          <Select options = {options} onChange={this.change.bind(this)} />
       </div>
+
     );
 
   }
-  componentDidMount() {
+
+    change(event) {
+        // event.persist(); //THE MAIN LINE THAT WILL SET THE VALUE
+        console.log("changed to " + event.value);
+        selectedStream = event.value
+        var video = document.getElementById('myplayer');
+        var source = document.getElementById('source');
+        video.pause();
+        source.setAttribute('src',  selectedStream);
+        video.load();
+        video.play();
+    }
+
+    componentDidMount() {
       this.setState({ streams : theStreams});
       this.getStreams();
+      this.createOptions();
+  }
+
+  createOptions(){
+      options = [];
+      for (let i = 0; i < theStreams.length; i++) {
+          let option = {
+              value: theStreams[i],
+              label: theStreams[i]
+          }
+          options.push(option)
+      }
   }
 
     async getStreams() {
